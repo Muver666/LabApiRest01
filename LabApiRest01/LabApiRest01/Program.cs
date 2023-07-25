9316ffb49370c8bad54606c8e639103bae58e3ee
Miguel <miguel1013100300@gmail.com>
2023-07-25 16:51:33 -0500

    conf(backend): Add mapWhen method

--- LabApiRest01/LabApiRest01/Program.cs
+++ LabApiRest01/LabApiRest01/Program.cs
@@ -67,6 +67,12 @@ app.Map("/usingmapbranch", builder => {
 
 });
 
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder => {
+    builder.Run(async context => {
+        await context.Response.WriteAsync("Hello from the MapWhen branch.");
+    });
+});
+
 app.Run(async context => {
 
     Console.WriteLine($"Writing the response to the client in the Run method");
